bf02646831e1726511edb86c80c4c8681fa86f57
zdravkostanin1 <zdravko.stanin@gmail.com>
2023-10-16 10:36:14 +0300

    bloc implementations

--- lib/timer/bloc/timer_bloc.dart
+++ lib/timer/bloc/timer_bloc.dart
@@ -5,10 +5,11 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 part 'timer_event.dart';
 part 'timer_state.dart';
 
+// TODO: Create a new branch --> for bloc implementation in git
 class TimerBloc extends Bloc<TimerEvent, TimerState> {
-  TimerBloc() : super(TimerInitial()) {
-    on<TimerEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+  static const int _duration = 60;
+
+  TimerBloc() : super(const TimerInitial(_duration)) {
+    // TODO: implement event handlers
   }
 }
--- lib/timer/bloc/timer_event.dart
+++ lib/timer/bloc/timer_event.dart
@@ -3,3 +3,45 @@ part of 'timer_bloc.dart';
 abstract class TimerEvent extends Equatable {
   const TimerEvent();
 }
+
+final class TimerStarted extends TimerEvent {
+  const TimerStarted({required this.duration});
+  final int duration;
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => throw UnimplementedError();
+}
+
+final class TimerPaused extends TimerEvent {
+  const TimerPaused();
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => throw UnimplementedError();
+}
+
+final class TimerResumed extends TimerEvent {
+  const TimerResumed();
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => throw UnimplementedError();
+}
+
+class TimerReset extends TimerEvent {
+  const TimerReset();
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => throw UnimplementedError();
+}
+
+class _TimerTicked extends TimerEvent {
+  const _TimerTicked({required this.duration});
+  final int duration;
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => throw UnimplementedError();
+}
